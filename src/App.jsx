93a42e81d1c4d49a642acf9a7e75dc4902a84150
George Appianing <george.adu.appianing@gmail.com>
2025-04-26 23:25:40 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -101,16 +101,17 @@ function Search() {
 }
 
 function Main() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
   return (
     <main className="main">
+      <ListBox />
+      <WatchedBox />
+    </main>
+  );
+
+  function ListBox() {
+    const [isOpen1, setIsOpen1] = useState(true);
+
+    return (
       <div className="box">
         <button
           className="btn-toggle"
@@ -118,24 +119,45 @@ function Main() {
         >
           {isOpen1 ? "–" : "+"}
         </button>
-        {isOpen1 && (
-          <ul className="list">
-            {movies?.map((movie) => (
-              <li key={movie.imdbID}>
-                <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                <h3>{movie.Title}</h3>
-                <div>
-                  <p>
-                    <span>🗓</span>
-                    <span>{movie.Year}</span>
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        )}
+        {isOpen1 && <MovieList />}
       </div>
+    );
+  }
 
+  function MovieList() {
+    const [movies, setMovies] = useState(tempMovieData);
+
+    return (
+      <ul className="list">
+        {movies?.map((movie) => (
+          <Movie movie={movie} key={movie.imdbID} />
+        ))}
+      </ul>
+    );
+  }
+
+  function Movie({ movie }) {
+    return (
+      <li key={movie.imdbID}>
+        <img src={movie.Poster} alt={`${movie.Title} poster`} />
+        <h3>{movie.Title}</h3>
+        <div>
+          <p>
+            <span>🗓</span>
+            <span>{movie.Year}</span>
+          </p>
+        </div>
+      </li>
+    );
+  }
+
+  function WatchedBox() {
+    const [watched, setWatched] = useState(tempWatchedData);
+    const [isOpen2, setIsOpen2] = useState(true);
+    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+    const avgUserRating = average(watched.map((movie) => movie.userRating));
+    const avgRuntime = average(watched.map((movie) => movie.runtime));
+    return (
       <div className="box">
         <button
           className="btn-toggle"
@@ -192,6 +214,6 @@ function Main() {
           </>
         )}
       </div>
-    </main>
-  );
+    );
+  }
 }
