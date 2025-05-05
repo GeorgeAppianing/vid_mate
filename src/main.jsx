ee893db09f53e718332a8e274be2edaa604fac35
George Appianing <george.adu.appianing@gmail.com>
2025-05-05 22:46:21 +0000

    added ratings

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import StarRating from "./StarRating.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
     <StarRating maxRating={10} />
-    <StarRating />
   </StrictMode>
 );
